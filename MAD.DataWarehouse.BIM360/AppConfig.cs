710c55c19d3551f38257be503e9f9c5c3f59d71e
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-08-31 08:08:13 +1000

    Add project files.

---
+++ MAD.DataWarehouse.BIM360/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAD.DataWarehouse.BIM360
+{
+    public class AppConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ MAD.DataWarehouse.BIM360/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MIFCore.Settings;
+
+namespace MAD.DataWarehouse.BIM360
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection serviceDescriptors)
+        {
+            serviceDescriptors.AddIntegrationSettings<AppConfig>();
+        }
+
+        public void Configure()
+        {
+
+        }
+    }
+}
